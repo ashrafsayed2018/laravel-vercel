e23941f398b4e12653a43a0ec68a1d869351cb2c
nql-afesh <monaalii35094@gmail.com>
2024-10-18 11:59:26 +0300

    add dist directory to build in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    build: {
+        outDir: 'dist', // Set the output directory to 'dist'
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-});
+})
